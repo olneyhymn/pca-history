bfa467724d1ee0ef1a9bb8a9ab94bcefb467c66f
Tim Hopper <thopper@cylance.com>
2019-08-07 16:17:16 -0400

    set user agent

--- tweet.py
+++ tweet.py
@@ -84,7 +84,14 @@ def update_twitter(title, url, image_url):
     status = f"{title} {url} #PCAhistory"
     try:
         if image_url is not None:
-            image = urllib.request.urlopen(image_url, timeout=10).read()
+            req = urllib.request.Request(
+                url,
+                data=None,
+                headers={
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+                }
+            )
+            image = urllib.request.urlopen(req, timeout=10).read()
             media = t.media.upload(media=image)
             t.statuses.update(status=status, media_ids=media['media_id_string'])
         else:
